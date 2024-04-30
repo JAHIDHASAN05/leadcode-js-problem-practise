f1a3057ed3bdce462a5a82129b83b94dd29a6174
JAHID HASAN <developerjahid@gmail.com>
2024-04-30 09:46:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("jahid is the best")
+// 16. 3Sum Closest
+// Medium
+// Topics
+// Companies
+// Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+// Return the sum of the three integers.
+
+// You may assume that each input would have exactly one solution.
+
+
+const nums = [-1,2,1,-4]
+
+
+const target=1
